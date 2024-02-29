87167e7f13bdace4efcb58e2d771bedb0a9bd87a
nidhig77 <98266379+nidhig77@users.noreply.github.com>
2024-02-29 15:05:37 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,13 @@
+# Python code for printing "Hey Nikita"
+def greet_person(name):
+    """
+    Function to greet a person by their name.
+    
+    Parameters:
+    - name (str): The name of the person to greet.
+    """
+    greeting = "Hey " + name
+    print(greeting)
+
+# Calling the function with the name "Nikita"
+greet_person("Nikita")
